b51a2a5bc0c90e8e359b420c7f1fdc32b033be01
SK SAHINUR ISLAM <67267702+GeniousSonu@users.noreply.github.com>
2023-04-03 19:35:20 +0530

    Add files via upload

---
+++ TEAM GENIOUS - Experiment/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing" , {
+    strings:["Website", "Android App", "Webapp", "Logo Design", "Graphics Design", "Content Writing","Website"],
+    typeSpeed:100,
+    BackSpeed:65,
+    loop:true
+})
